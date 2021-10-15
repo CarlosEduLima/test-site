c4ec5681cd86a4ba8c755fc97b7060d2ab8bf230
BrunoMoraes <brunomoraes.ti@gmail.com>
2021-10-15 11:34:32 -0300

    added placeholders

---
+++ src/helpers/placeholder.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const placeholder = () => {
+  return (
+    <div>
+      Assim que adicionar algum arquivo nessa pasta, me exclua. Estou aqui como placeholder, para
+      enviar a pasta para o GitHub.
+    </div>
+  );
+};
+
+export default placeholder;
