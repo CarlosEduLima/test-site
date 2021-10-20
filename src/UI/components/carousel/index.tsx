a4cf5e86098476072132fb26e5d88fd18772dff8
Guilherme A. Kehl <guilherme@kehl.eng.br>
2021-10-20 18:11:41 -0300

    feat: implement scrolling and dragging

--- src/UI/components/carousel/index.tsx
+++ src/UI/components/carousel/index.tsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Container, Divider, HorizontalScrollArea, ScrollAreaContainer, Title } from './styles'
 import { InputCard } from './input-card'
+import useDraggableScroll from 'use-draggable-scroll';
 
 export const Carousel: React.FC = () => {
+  const ref = useRef(null);
+  const { onMouseDown } = useDraggableScroll(ref);
+
+  function onWheel(ev: React.WheelEvent): void {
+    if (ref.current) {
+      let initialPosition = {
+        scrollLeft: ref.current.scrollLeft,
+      }
+      ref.current.scrollLeft = initialPosition.scrollLeft + ev.deltaY
+    }
+    console.log(ev)
+  }
+
   return (
     <Container>
       <Title style={{ marginBottom: 12 }}>Categorias em destaque</Title>
       <Divider></Divider>
     <ScrollAreaContainer>
-      <HorizontalScrollArea>
+        <HorizontalScrollArea ref={ref} onWheel={(onWheel)} onMouseDown={onMouseDown}>
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
           <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
       </HorizontalScrollArea>
       </ScrollAreaContainer>
