7a7fcfb314d1e1305c4015a14180fa7cdc589d19
Guilherme A. Kehl <guilherme@kehl.eng.br>
2021-10-26 08:52:59 -0300

    feat: add get services highlight api call

--- src/UI/components/carousel/index.tsx
+++ src/UI/components/carousel/index.tsx
@@ -1,18 +1,23 @@
-import React, { useRef } from 'react';
-import { Container, Divider, HorizontalScrollArea, ScrollAreaContainer, Title } from './styles'
-import { InputCard } from './input-card'
+import React, { useEffect, useRef } from 'react';
+import { Container, Divider, HorizontalScrollArea, ScrollAreaContainer, Title } from './styles';
+import { InputCard } from './inputCard';
 import useDraggableScroll from 'use-draggable-scroll';
+import { ServicesHighlight } from '../../../services/services';
 
 export const Carousel: React.FC = () => {
   const ref = useRef(null);
   const { onMouseDown } = useDraggableScroll(ref);
 
+  useEffect(() => {
+    void ServicesHighlight().then(console.log).catch(console.log);
+  }, []);
+
   function onWheel(ev: React.WheelEvent): void {
     if (ref.current) {
-      let initialPosition = {
+      const initialPosition: { scrollLeft: number } = {
         scrollLeft: ref.current.scrollLeft,
-      }
-      ref.current.scrollLeft = initialPosition.scrollLeft + ev.deltaY * 3
+      };
+      ref.current.scrollLeft = initialPosition.scrollLeft + ev.deltaY * 3;
     }
   }
 
@@ -20,25 +25,25 @@ export const Carousel: React.FC = () => {
     <Container>
       <Title style={{ marginBottom: 12 }}>Categorias em destaque</Title>
       <Divider></Divider>
-    <ScrollAreaContainer>
-        <HorizontalScrollArea ref={ref} onWheel={(onWheel)} onMouseDown={onMouseDown}>
-          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
-          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
-          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
-          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
-          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
-          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
-          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
-          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
-          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
-          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
-          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
-          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
-          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
-          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151"/>
-      </HorizontalScrollArea>
+      <ScrollAreaContainer>
+        <HorizontalScrollArea ref={ref} onWheel={onWheel} onMouseDown={onMouseDown}>
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151" />
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151" />
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151" />
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151" />
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151" />
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151" />
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151" />
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151" />
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151" />
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151" />
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151" />
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151" />
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151" />
+          <InputCard title="Auto" backgroundImage="https://source.unsplash.com/random/325x151" />
+        </HorizontalScrollArea>
       </ScrollAreaContainer>
       <button>Quero me cadastrar</button>
     </Container>
-  )
-}
+  );
+};
