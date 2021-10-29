8454aacb4ba1051245ac69fb33fdf34f00e501f2
jhony2488 <jhon.araujo2488@gmail.com>
2021-10-29 11:24:12 -0300

    .

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,6 +1,4 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable prettier/prettier */
 
-export function filterPreviewSearch(){
-
-}
+export function filterPreviewSearch(datas: any, data: any) {}
