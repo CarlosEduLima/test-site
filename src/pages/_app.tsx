bc87bbd29a6244653f6777dea6a5663e35546456
Guilherme A. Kehl <guilherme@kehl.eng.br>
2021-11-04 13:38:12 -0300

    fix: add return and argument type on _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import GlobalStyle from '../utils/globals';
 import '../assets/css/index.css';
+import { AppProps } from 'next/app';
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
       <GlobalStyle />
