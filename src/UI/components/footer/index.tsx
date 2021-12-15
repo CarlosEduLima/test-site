1ebe103eb9e3181f328e5ec2dde991cfc552fe01
Gustavo Schultz <gustavo.schulr@gmail.com>
2021-12-15 16:42:51 -0300

    Footer inteiro e imagens necessarias

--- src/UI/components/footer/index.tsx
+++ src/UI/components/footer/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 import {
@@ -8,7 +9,18 @@ import {
   TextDown,
   FooterSection,
   FooterList,
+  FooterListImg,
+  ImageLocal,
+  ImageLocalDiv,
+  DownloadImage,
 } from './styles';
+import instagram from '../../../assets/instagram.png';
+import facebook from '../../../assets/facebook.png';
+import twitter from '../../../assets/twitter.png';
+import linkedin from '../../../assets/linkedin.png';
+import googlePlay from '../../../assets/googlePlay.png';
+import appStore from '../../../assets/appStore.png';
+import iziwIcon from '../../../assets/icon.png';
 
 const Footer = () => {
   const categoriasItens = [
@@ -23,17 +35,87 @@ const Footer = () => {
     'Saúde',
     'Serviços Domésticos',
   ];
+  const institucionalItens = ['Quem Somos', 'Trabalhe Conosco', 'Profissionais Verificados'];
+  const imagensSociais = [
+    {
+      name: 'instagram',
+      local: instagram,
+    },
+    {
+      name: 'facebook',
+      local: facebook,
+    },
+    {
+      name: 'twitter',
+      local: twitter,
+    },
+    {
+      name: 'linkedin',
+      local: linkedin,
+    },
+  ];
+  const footerFAQ = [
+    'Ajuda',
+    'Termos de Uso',
+    'Firedev It Solutions 2021 - Todos os Direitos Reservados',
+  ];
+
   return (
     <FooterContainer>
       <FooterUp>
-        <FooterSection>
+        <FooterSection width="300px">
           <TextUp>Categorias</TextUp>
           <FooterList>
             {categoriasItens.map(item => <TextDown>{item}</TextDown>)}
           </FooterList>
         </FooterSection>
+        <FooterSection>
+          <TextUp>Institucional</TextUp>
+          <FooterList>
+            {institucionalItens.map(item => <TextDown>{item}</TextDown>)}
+          </FooterList>
+        </FooterSection>
+        <FooterSection>
+          <TextUp>Redes Sociais</TextUp>
+          <FooterList>
+            <FooterListImg>
+              {imagensSociais.map((item) => (
+                <ImageLocalDiv key={item.name}>
+                  <ImageLocal>
+                    {item.name === 'facebook' ? (
+                      <Image src={item.local} width={15} height={30} />
+                    ) : (
+                      <Image src={item.local} height={500} />
+                    )}
+                  </ImageLocal>
+                </ImageLocalDiv>
+              ))}
+            </FooterListImg>
+            <TextUp>Parcerias</TextUp>
+          </FooterList>
+        </FooterSection>
+        <FooterSection>
+          <TextUp>Downloads</TextUp>
+          <FooterList>
+            <DownloadImage>
+              <Image src={googlePlay} width={150} height={50} />
+            </DownloadImage>
+            <DownloadImage>
+              <Image src={appStore} width={150} height={50} />
+            </DownloadImage>
+          </FooterList>
+        </FooterSection>
       </FooterUp>
-      <FooterDown></FooterDown>
+      <FooterDown>
+        <Image
+          src={iziwIcon}
+        />
+        <FooterListImg>
+          {footerFAQ.map(item => (
+            <TextDown>{item}</TextDown>
+          ))}
+        </FooterListImg>
+      </FooterDown>
     </FooterContainer>
   )
 };
--- src/UI/components/footer/styles.ts
+++ src/UI/components/footer/styles.ts
@@ -2,26 +2,28 @@ import styled from 'styled-components';
 import colors from '../../../utils/colors';
 
 export const FooterContainer = styled.div`
+  text-decoration: none;
   box-sizing: border-box;
   width: 100%;
   padding: 40px;
-  background-color: #fff;
-  background: #f22;
+  background-color: ${colors.white};
   display: flex;
   flex-direction: column;
-  align-itens: center;
 `;
 
 export const FooterUp = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
 `;
 
 export const FooterDown = styled.div`
+  padding: 40px 0;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
+  flex-wrap: wrap;
 `;
 
 export const TextUp = styled.p`
@@ -41,6 +43,11 @@ export const TextDown = styled.p`
 export const FooterSection = styled.div`
   display: flex;
   flex-direction: column;
+  flex-wrap: wrap;
+  padding: 1px;
+  padding: 10px;
+  margin: 15px;
+  width: ${props => props.width};
 `;
 
 export const FooterList = styled.div`
@@ -48,5 +55,35 @@ export const FooterList = styled.div`
   flex-direction: column;
   align-itens: space-between;
   flex-wrap: wrap;
-  max-height: 250px;
+  max-height: 220px;
+`;
+
+export const FooterListImg = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+`;
+
+export const ImageLocalDiv = styled.div`
+  position: relative;
+  cursor: pointer;
+  display: flex;
+  margin: 8px 10px;
+  justify-content: center;
+  align-itens: center;
+  border-radius: 50%;
+  width: 10px;
+  height: 10px;
+  padding: 20px;
+  background-color: #29ABE2;
+`;
+
+export const ImageLocal = styled.div`
+  position: absolute;
+  top: 55%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+export const DownloadImage = styled.div`
+  margin: 10px 0;
 `;
