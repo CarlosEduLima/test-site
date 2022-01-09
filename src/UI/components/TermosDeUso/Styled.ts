9caf7f9a2a86b203df626f3ccfecf6e746562b22
Guilherme Sotério <guilhermehenrique.oliv3@gmail.com>
2022-01-09 20:09:53 -0300

    Corrções de Prettier EsLint e nomenclaturas

--- src/UI/components/TermosDeUso/Styled.ts
+++ src/UI/components/TermosDeUso/Styled.ts
@@ -1,16 +1,14 @@
-import styled from "styled-components"
-import colors from "src/utils/colors";
-import fonts from "src/utils/fonts";
+import styled from 'styled-components';
+import fonts from '../../../utils/fonts';
 
-
-export const Container2 = styled.div`
+export const ContainerTitle = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
 `;
 
-export const Titulo = styled.h2`
+export const Title = styled.h2`
   padding-bottom: 20px;
   font: ${fonts.bold};
   cursor: pointer;
@@ -20,17 +18,16 @@ export const Titulo = styled.h2`
   width: 100%;
   font-size: 35.81px;
 `;
-export const Borda = styled.div`
-    padding-bottom: 40px;
-    border-top: 3.07692px solid #29ABE2;
-    width: 200px;
+export const Border = styled.div`
+  padding-bottom: 40px;
+  border-top: 3.07692px solid #29abe2;
+  width: 200px;
 `;
 
 export const Container = styled.div`
   padding: 15px 10% 100px;
   font: ${fonts.regular};
-  font-size: 35,81px;
-  font-color:blue;
+  font-size: 35, 81px;
   cursor: pointer;
   display: flex;
   justify-content: center;
