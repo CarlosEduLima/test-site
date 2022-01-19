db05ce54cfa54adf48990a795bee650a017688f1
andressamonteirocarvalho <andressa18monteiro@gmail.com>
2022-01-19 15:50:24 -0300

    Criado Componente banner  de quem sonos

--- src/pages/whoWeAre/index.tsx
+++ src/pages/whoWeAre/index.tsx
@@ -1,79 +1,16 @@
 import React from 'react';
-import Link from 'next/link';
-import {
-  Container,
-  ContentLine,
-  Text,
-  Divider,
-  SubTitle,
-  Title,
-  Image,
-  SubContentRight,
-  SubContentLeft,
-  Top,
-  StyledLink,
-} from './styles';
-import top from '../../assets/top.png';
-import askingQuestions from '../../assets/asking-questions.png';
-import startupIdea from '../../assets/startup-idea.png';
-import workRiskFree from '../../assets/work-risk-free.png';
+import { Top, TextWhite, SubTitleWhite, TopCenter } from './styles';
 
 export const WhoWeAre: React.FC = () => {
   return (
-    <Container>
-      <Top>
-        <SubTitle>Quem somos</SubTitle>
-        <Text>
+    <Top>
+      <TopCenter>
+        <SubTitleWhite>Quem somos</SubTitleWhite>
+        <TextWhite>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
           ut labore.
-        </Text>
-      </Top>
-      <Title>A Iziw</Title>
-      <Divider />
-      <ContentLine>
-        <SubContentLeft>
-          <Image src={askingQuestions} alt="" />
-        </SubContentLeft>
-        <SubContentRight>
-          <SubTitle>Quem somos</SubTitle>
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Voluptate velit
-            esse cillum dolore eu fugiat nulla pariatur.
-          </Text>
-        </SubContentRight>
-      </ContentLine>
-
-      <ContentLine>
-        <SubContentLeft>
-          <SubTitle>Como surgiu</SubTitle>
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Voluptate velit
-            esse cillum dolore eu fugiat nulla pariatur.
-          </Text>
-        </SubContentLeft>
-        <SubContentRight>
-          <Image src={workRiskFree} alt="" />
-        </SubContentRight>
-      </ContentLine>
-
-      <ContentLine>
-        <SubContentLeft>
-          <Image src={startupIdea} alt="" />
-        </SubContentLeft>
-        <SubContentRight>
-          <SubTitle>O que fazemos</SubTitle>
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Voluptate velit
-            esse cillum dolore eu fugiat nulla pariatur.
-          </Text>
-        </SubContentRight>
-      </ContentLine>
-    </Container>
+        </TextWhite>
+      </TopCenter>
+    </Top>
   );
 };
--- src/pages/whoWeAre/styles.ts
+++ src/pages/whoWeAre/styles.ts
@@ -8,62 +8,30 @@ export const Top = styled.div`
   width: 100%;
   height: 490px;
   background: linear-gradient(#373aadaa, #373aadaa), url(${top.src}) no-repeat;
+  display: flex;
+  justify-content: center;
+  background-size: cover;
 `;
 
-export const Title = styled.h1`
-  font-family: ${fonts.extraBold};
-  color: ${colors.black};
-  font-size: 30px;
-`;
-export const Container = styled.div`
-  margin: 45.5px auto 64px auto;
+export const TopCenter = styled.div`
+  width: 1200px;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  padding: 60px;
 `;
-export const Divider = styled.div`
-  width: 124px;
-  height: 0;
-  border: 1px solid ${colors.LightBlue};
-`;
-export const ContentLine = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  align-items: center;
-`;
-export const SubContent = styled.div`
-  align-self: flex-end;
-  flex: 1;
+
+export const TextWhite = styled.p`
+  font-size: 20px;
+  line-height: 34px;
+  font-family: ${fonts.regular};
+  color: ${colors.white};
   max-width: 500px;
 `;
-export const SubContentLeft = styled(SubContent)`
-  text-align: right;
-`;
-export const SubContentRight = styled(SubContent)`
-  text-align: left;
-`;
-export const SubTitle = styled.h1`
-  font-size: 25px;
-  font-family: ${fonts.bold};
-  color: ${colors.purple};
-`;
-export const Text = styled.p`
-  font-size: 25px;
-  line-height: 34px;
+
+export const SubTitleWhite = styled.h1`
+  font-size: 30px;
   font-family: ${fonts.heading};
-  color: ${colors.black};
-  margin-bottom: 55px;
-`;
-export const StyledLink = styled.a`
-  font-size: 25px;
-  font-family: ${fonts.heading};
-  color: ${colors.black};
-  text-decoration: none;
-  text-decoration: underline;
-  cursor: pointer;
-`;
-export const Image = styled(NextImage)`
-  width: 545px;
+  color: ${colors.white};
+  margin-bottom: 40px;
 `;
