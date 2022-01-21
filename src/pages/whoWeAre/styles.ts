f1846eb59ee3650b5b54d4268538e8ab7629021e
andressamonteirocarvalho <andressa18monteiro@gmail.com>
2022-01-21 17:33:36 -0300

    Página do Quem somos

--- src/pages/whoWeAre/styles.ts
+++ src/pages/whoWeAre/styles.ts
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
 import colors from '../../utils/colors';
 import fonts from '../../utils/fonts';
-import NextImage from 'next/image';
 import top from '../../assets/top.png';
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  position: absolute;
+  z-index: 9999;
+`;
+export const NewsletterContainer = styled.div`
+  margin-top: 100px;
+`;
 
 export const Top = styled.div`
   width: 100%;
