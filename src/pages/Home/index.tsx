1f1b6c91628272828a867f8a53f7afb5bc993375
andressamonteirocarvalho <andressa18monteiro@gmail.com>
2022-01-27 12:54:48 -0300

    Commit parcial da contrução da pagina Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Card from 'src/UI/components/Card-cadastro';
+import { WhatMakesUsDifferent } from '../../UI/components/whatMakesUsDifferent';
+import Footer from '../../UI/components/footer';
+import { Newsletter } from '../../UI/components/newsletter';
+import { AvailableInStores } from '../../UI/components/availableInStores';
+import { Carousel } from 'src/UI/components/carousel';
 
 import HowItWorks from '../../UI/components/howItWorks';
 
 const Home: React.FC = () => {
   return (
     <div>
+      <Carousel />
       <HowItWorks />
+      <AvailableInStores />
+      <WhatMakesUsDifferent />
+      <Newsletter />
+      <Footer />
     </div>
   );
 };
