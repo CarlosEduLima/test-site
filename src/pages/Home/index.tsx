234b32e3b3cc4f9d47cb0a4cd4ee4f8408a524df
andressamonteirocarvalho <andressa18monteiro@gmail.com>
2022-01-27 19:04:50 -0300

    Montagem da página Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import Card from 'src/UI/components/Card-cadastro';
 import { WhatMakesUsDifferent } from '../../UI/components/whatMakesUsDifferent';
 import Footer from '../../UI/components/footer';
 import { Newsletter } from '../../UI/components/newsletter';
 import { AvailableInStores } from '../../UI/components/availableInStores';
-import { Carousel } from 'src/UI/components/carousel';
 
 import HowItWorks from '../../UI/components/howItWorks';
+import { Header } from '../../UI/components/Header';
+import { HeaderContainer } from './styles';
+import { HighProfessionals } from '../../UI/components/highProfessionals';
+import { Carousel } from 'src/UI/components/carousel';
 
 const Home: React.FC = () => {
   return (
     <div>
+      <HeaderContainer>
+        <Header />
+      </HeaderContainer>
+      <HighProfessionals />
       <Carousel />
       <HowItWorks />
       <AvailableInStores />
