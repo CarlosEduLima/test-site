cdd1b0f052d40b8710191ad3d66a10ed968b3a1c
cesarvargasjr <cesar_vargas_jr@hotmail.com>
2022-01-28 12:23:45 -0300

    Criação tela de login

---
+++ src/UI/components/login/index.tsx
@@ -0,0 +1,111 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable react/no-children-prop */
+import React, { useState } from 'react';
+import LogoImg from '../../../assets/icon.png';
+import { LoginUser, ILoginUserProps } from 'src/services/auth';
+import * as yup from 'yup';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import {
+    BoxButton,
+    BoxInput,
+    Logo,
+    Box,
+    Container,
+    ContainerText,
+    Text,
+    styleInput,
+    styleError,
+    ErrorLogin,
+} from './styles';
+import { Button } from '../Button';
+
+export default function Login() {
+    const [data, setData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const [resp, setResp] = useState({});
+    console.log(resp === {})
+
+    async function makeRequest(formData: ILoginUserProps) {
+        try {
+            const resposta = await LoginUser(formData);
+
+            return resposta;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const userSchema = yup.object({
+        email: yup
+            .string()
+            .email('Email inválido')
+            .required('Preencha o campo de e-mail'),
+        password: yup
+            .string()
+            .required('Preencha o campo da senha'),
+        // .oneOf([yup.ref('password'), null], 'Senha incorreta')
+    });
+
+    const handleSubmit = async (values) => {
+        setData({ ...values })
+        console.log(values)
+
+        const resposta = await makeRequest(values)
+        setResp(resposta)
+        console.log(resposta, 'RESPOSTA MAKE_REQUEST')
+    }
+
+    return (
+        <Container>
+            <Formik validationSchema={userSchema} initialValues={data} onSubmit={handleSubmit}>
+                {({ errors }) => (
+                    <Box>
+                        <Form>
+                            <Logo src={LogoImg} alt="Search" />
+                            <ContainerText>
+                                <Text>
+                                    Faça seu Login
+                                </Text>
+                            </ContainerText>
+
+                            <BoxInput>
+                                <Field name="email" style={styleInput(errors.email)} placeHolder="Email" />
+                                <ErrorMessage name="email">
+                                    {(msg) => <div style={styleError}>{msg}</div>}
+                                </ErrorMessage>
+                            </BoxInput>
+
+                            <BoxInput>
+                                <Field
+                                    name="password"
+                                    style={styleInput(errors.password)}
+                                    type="password"
+                                    placeHolder="Senha"
+                                />
+                                <ErrorMessage name="password">
+                                    {(msg) => <div style={styleError}>{msg}</div>}
+                                </ErrorMessage>
+                            </BoxInput>
+
+                            <BoxButton>
+                                <Button
+                                    variant={'primary'}
+                                    children={'Prosseguir'}
+                                    height={'54px'}
+                                    widthCircle={''}
+                                    heightCircle={''}
+                                />
+                            </BoxButton>
+                            {resp === undefined && (<ErrorLogin>E-mail ou senha incorretos</ErrorLogin>)}
+                        </Form>
+                    </Box>
+                )}
+            </Formik>
+        </Container>
+    );
+};
---
+++ src/UI/components/login/styles.ts
@@ -0,0 +1,105 @@
+import styled from 'styled-components';
+import Image from 'next/image';
+import fonts from 'src/utils/fonts';
+import colors from 'src/utils/colors';
+import img from '../../../assets/imagemFundoCadastro.png';
+
+export const Container = styled.div`
+  background: url(${img.src}) no-repeat;
+  background-size: cover;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+  position: relative;
+`;
+
+export const Logo = styled(Image)`
+  margin-bottom: 30px;
+  display: flex;
+`;
+
+export const Text = styled.h3`
+  font: ${fonts.regular};
+  font-size: 27px;
+  font-weight: 600;
+`;
+
+export const ContainerText = styled.div`
+  padding-bottom: 30px;
+  padding-top: 30px;
+`;
+
+export const styleInput = (isError) => {
+  return {
+    paddingLeft: '25px',
+    borderRadius: '50px',
+    border: isError ? `${colors.redError} solid 1px` : '#f4fafd',
+    display: 'flex',
+    backgroundColor: '#f4fafd',
+    width: '357px',
+    height: '50px',
+    marginBottom: '0em',
+    marginTop: '0.4em',
+    outline: 'none !important',
+    boxShadow: '0  0 0 2px #98887',
+    color: 'black',
+  };
+};
+
+export const styleError = {
+  color: `${colors.redError}`,
+  marginTop: 6,
+};
+
+export const ErrorLogin = styled.p`
+    color: ${colors.redError};
+    font-family: ${fonts.heading};
+    margin-top: 15px;
+  `;
+
+export const Input = styled.input`
+  box-shadow: none !important;
+  padding: 0.5em;
+  background: #f4fafd;
+  border: none !important;
+  border-radius: 3px;
+  width: 357px;
+  height: 50px;
+  margin-bottom: 1.5em;
+  margin-top: 0.4em;
+`;
+
+export const Box = styled.div`
+  background-color: white;
+  width: 458px;
+  height: 612px;
+  position: absolute;
+  bottom: -25px;
+  right: 55%;
+  display: flex;
+  flex-direction: column;
+  border-radius: 25px;
+  padding: 5rem 1.5rem;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+
+  @media (max-width: 1100px) {
+    top: 60%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+`;
+
+export const BoxInput = styled.div`
+  border-radius: 50%;
+  color: '#F00';
+  flex-direction: column;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 10px;
+`;
+
+export const BoxButton = styled.div`
+  padding-top: 15px;
+`;
