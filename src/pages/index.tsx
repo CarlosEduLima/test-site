43ebc982beb4af0d283a1d64947fa10f1efc06ce
Carlos Lima <cadufait124@gmail.com>
2022-10-19 10:47:31 -0300

    fix index.jsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import Home from '../pages/Home';
-import Input from 'src/UI/components/Input';
 
 const Index: React.FC = () => {
   return (
     <div>
-      <Input
-        label="Teste"
-        name="Input"
-        icon="password"
-        typeInput="password"
-        placeholder="Teste"
-        dataSearch={['Gustavo', 'Teste']}
-      />
-      {/* <Home /> */}
+      <Home />
     </div>
   );
 };
