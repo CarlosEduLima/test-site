d36dbbae35d14246aaebc3156786ac730caf42c3
Santana <santana@firedev.com.br>
2022-11-17 13:52:21 -0300

    Toast container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,15 @@ import GlobalStyle from '../utils/globals';
 import '../assets/css/index.css';
 import { AppProps } from 'next/app';
 
+import { ToastProvider } from 'src/hooks/toast';
+
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <ToastProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ToastProvider>
     </>
   );
 };
